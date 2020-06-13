c4cf952f16f560b7783747c73684510eb41851c1
G'ayrat <risqulovgayrat96@gmail.com>
2020-06-13 12:44:38 +0500

    update
    update

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-function calculateBmi () {
-    const name = document.getElementById("name");
-    const weightVal = document.getElementById("weight");
-    const heightVal = document.getElementById("height");
-    const result = document.querySelector("#result");
+function calculateBmi() {
+  var weight = document.getElementById("weight").value;
+  var height = document.getElementById("height").value;
 
-    const weight = parseFloat(weightVal.value);
-    const height = parseFloat(heightVal.value);
-    
-    let bmi = (weight / (height * height)).toFixed(2);
+  var newbmivalue = weight / (height * height);
 
-       /* if (bmi <= 18.5) {
+  /* if (bmi <= 18.5) {
             console.log("Underweight");
         } else if ( bmi = 18.5 && bmi < 24.9) {
             console.log("Normal weight");
@@ -17,8 +12,6 @@ function calculateBmi () {
             console.log("Overweight");
         } else (bmi >= 30 )  
             console.log("Obesity"); */
-       
 
-    result.innerHTML = ` ${name.value}'s BMI is ${bmi}. You result is`
-
+  document.getElementById("result").innerHTML = "Your BMI is " + newbmivalue;
 }
